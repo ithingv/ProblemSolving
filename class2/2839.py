92d5136393f3da0959fa44fe3e6c69ec9b77b47f
sanghoon <ithingvv@.naver.com>
2021-05-05 00:51:13 +0900

    2839.py

---
+++ class2/2839.py
@@ -0,0 +1,24 @@
+def sugar_delivery(N):
+    res = []
+    Y = 0
+    
+    while (N - 5*Y) > 0:
+        
+        if (N - 5*Y) % 5 == 0:
+            res.append((N - 5*Y) // 5)
+            break
+            
+        if (N - 5*Y) % 3 == 0:
+            X = (N - 5 * Y) // 3
+            res.append(X + Y)
+        
+        Y += 1
+
+    return res
+
+N = int(input())
+
+if len(sugar_delivery(N)) == 0:
+    print(-1)
+else:
+    print(min(sugar_delivery(N)))
