ec8ff8ede5dba2de754a96b5bd0123c5ffff7cdf
name <ithingvv@naver.com>
2021-05-05 22:55:47 +0900

    class2

---
+++ class2/1436.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+i = 0
+j = 0
+while True:
+    
+    if '666' in str(i):
+        j += 1
+        if j == N:
+            print(i)
+            break
+    i += 1
---
+++ class2/15829.py
@@ -0,0 +1,12 @@
+L = int(input())
+word = input()
+res = 0
+cnt = 0
+
+for alpha in word:
+    res += (ord(alpha) - 96) * pow(31, cnt)
+    if res > 2147483647:
+        res %= 1234567891
+    cnt += 1
+
+print(res)
