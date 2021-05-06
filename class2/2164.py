bff6e6e7d31ec4a58c0c080f686b6ebe06391d11
name <ithingvv@naver.com>
2021-05-06 23:12:33 +0900

    class2

---
+++ class2/2164.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+N = int(input())
+
+deque = deque([i for i in range(1, N+1)])
+flag = True
+
+while True:
+
+    if len(deque) == 1:
+        print(deque[0])
+        break
+        
+    if flag == True:
+        deque.popleft()
+        flag = False
+    else:
+        deque.append(deque.popleft())
+        flag = True
---
+++ class2/9012.py
@@ -0,0 +1,35 @@
+#Silver IV 9012	괄호 ESSENTIAL	
+
+N = int(input())
+i = 0
+
+while i < N:
+    
+    j = 0
+    sentence = str(input())    
+    stack = []
+    valid = True
+    
+    while j < len(sentence):
+    
+        curr_char = sentence[j]
+        
+        if curr_char == '(': 
+            stack.append(curr_char)
+        if curr_char == ')':
+        
+            if len(stack) == 0:
+                valid = False
+                break
+            else:    
+                if stack[-1] != '(':
+                    valid = False
+                    break
+                else:
+                    stack.pop()
+        j += 1
+    if valid and len(stack) == 0:
+        print("YES")
+    else:
+        print("NO")    
+    i += 1
