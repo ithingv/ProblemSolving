7725c5ed34576e034b0d144b3e5e903ada69ac92
name <ithingvv@naver.com>
2021-05-08 20:24:40 +0900

    class 2

--- class2/1654.py
+++ class2/1654.py
@@ -1,1 +1,18 @@
 # 실버 3 랜선 자르기
+
+import sys
+
+K, N = map(int, sys.stdin.readline().split())
+line_list = [int(sys.stdin.readline()) for _ in range(K)]
+start, end = 1, max(line_list)
+
+while start <= end:
+    mid = (start + end) // 2
+    lines = 0
+    for line in line_list:
+        lines += line // mid
+    if lines >= N:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
--- class2/2108.py
+++ class2/2108.py
@@ -1,1 +1,33 @@
-# 실버 4 통계학
+# 실버 4 통계학#Silver IV 2108	통계학	
+from collections import Counter
+import sys
+
+N = int(input())
+i = 0
+arr = []
+while i < N:
+    arr.append(int(sys.stdin.readline()))
+    i += 1
+    
+# 산술평균
+print(round(sum(arr) / len(arr)))
+
+# 중앙값
+arr.sort()
+left, right = 0, len(arr) - 1
+print(arr[(left + right) // 2])
+
+# 최빈값
+counter = Counter(arr)
+mode = counter.most_common()
+
+if len(arr) > 1:
+    if mode[0][1] == mode[1][1]:
+        print(mode[1][0])
+    else:
+        print(mode[0][0])
+else:
+    print(mode[0][0])
+        
+# 범위
+print(max(arr) - min(arr))
