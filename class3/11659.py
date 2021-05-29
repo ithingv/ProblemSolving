4969105cb4a13dfecfd8ad876f7a2db633ea1e13
Sanghoon Lee <ithing43@gmail.com>
2021-05-29 20:55:09 +0900

    Update 11659.py

---
+++ class3/11659.py
@@ -0,0 +1,14 @@
+import sys
+from itertools import accumulate
+
+N, M = map(int, sys.stdin.readline().split())
+
+num = list(map(int, sys.stdin.readline().split()))
+num = list(accumulate(num))
+num.append(0)
+
+i = 0
+while i < M:
+    start, end = map(int, sys.stdin.readline().split())
+    print(num[end-1] - num[start-2])
+    i += 1
