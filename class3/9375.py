9920b06355fe890d40fdc0df96cada5c109c5ed3
sanghoon <ithing43@gmail.com>
2021-05-30 21:21:45 +0900

    class3

--- class3/9375.py
+++ class3/9375.py
@@ -1,6 +1,25 @@
-"""
-오늘 : 안경, 코트, 상의, 신발
-내일 : 
-"""
+import sys
 
-
+N = int(input())
+i = 0
+while i < N:
+    
+    n = int(input())
+    wear = {}
+    res = 1
+    j = 0
+    
+    while j < n:
+        name, category = map(str, sys.stdin.readline().split())
+        if category not in wear:
+            wear[category] = [name]
+        else:
+            wear[category].append(name)
+        j += 1
+    
+    for category in wear.keys():
+        res *= (len(wear[category]) + 1)
+    
+    print(res - 1)
+    del(wear)    
+    i += 1
