f1f3cdb9f9d30782ef55452143173c86a3e58a48
Sanghoon Lee <ithing43@gmail.com>
2021-06-24 20:29:40 +0900

    Update 1764.py

--- class3/1764.py
+++ class3/1764.py
@@ -4,7 +4,7 @@ N, M = map(int, sys.stdin.readline().split())
 N_list = [sys.stdin.readline().strip() for i in range(N)]
 M_list = [sys.stdin.readline().strip() for i in range(M)]
 
-# 교차하는 이름들을 찾는다
+# 교차하는 이름을 찾는다
 duplicate = list(set(N_list) & set(M_list))
 
 print(len(duplicate))
