981be03e35d15087829b0b12a8d279089845dd7e
Sanghoon Lee <ithingvv@naver.com>
2022-02-08 10:33:01 +0900

    upload

---
+++ solved.ac/class1/go/1000.go
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/1000
+package main
+
+import "fmt"
+
+func main(){
+	var a, b int
+	fmt.Scanf("%d %d", &a, &b)
+	fmt.Rrintln(a+b)
+}
---
+++ solved.ac/class1/go/10998.go
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/10998
+package main
+
+import "fmt"
+
+func main(){
+	var a, b int
+	fmt.Scanf("%d %d", &a, &b)
+	fmt.Println(a*b)
+}
